bc4a8d8eb8e99ce2fecbb03ff42721feffca4cfc
elmarckedu <tuzzo2306@outlook.com>
2017-10-23 11:15:09 -0500

    frutas 11:15

---
+++ frutas.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+int main()
+{
+int a;
+cout<<"selecciona un opcion:\n";
+cout<<"opcion 1 Rojo\n";
+cout<<"opcion 2 Amarillo\n";
+cout<<"opcion 3 Morado\n";
+cin>>a;
+	switch(a)
+	{	
+		case 1: cout<<"Manzana\n";
+		cout<<"fresa\n";
+		//agregar frutas
+		break;
+		case 2: cout<<"Platano\n";
+		break;
+		case 3: cout<<"Uva\n";
+		break;
+		
+		
+	
+}
+ system("pause");
+return 0;
+}
